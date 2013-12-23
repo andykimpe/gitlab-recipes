7f6a6d4d6f32063c093657774d35e92ea6666be9
Kimpe Andy <andykimpe@gmail.com>
2013-12-23 07:03:10 +0100

    Create uninstall-mysql.sh

---
+++ install/centos/uninstall-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+read -e -p "Enter the public (external) server IP: " password
+rm -f /etc/yum.repos.d/PUIAS_6_computational.repo
+service gitlab stop
+rm -f /etc/init.d/gitlab
+rm -rf /usr/local/lib/ruby
+rm -rf /usr/lib/ruby
+rm -f /usr/local/bin/ruby
+rm -f /usr/bin/ruby
+rm -f /usr/local/bin/irb
+rm -f /usr/bin/irb
+rm -f /usr/local/bin/gem
+rm -f /usr/bin/gem
+userdel git
+rm -rf /home/git
+rm -f /root/.forward
+mysql -u root -p$password -e "DROP DATABASE gitlabhq_production";
