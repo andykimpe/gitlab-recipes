baccbdafdd4060ab1e60382ad763abfae014b05f
Kimpe Andy <andykimpe@gmail.com>
2013-12-27 21:32:22 +0100

    Update install-mysql-apache.sh

--- install/centos/install-mysql-apache.sh
+++ install/centos/install-mysql-apache.sh
@@ -3,7 +3,7 @@
 logfile=/var/log/gitlab-install-mysql-apache.log
 exec > >(tee $logfile)
 exec 2>&1
-if [ $UID -ne 0 ]; then
+if [ $USER -ne root ]; then
 echo "Installed failed! To install you must be logged in as 'root', please try again"
   exit 1
 fi
